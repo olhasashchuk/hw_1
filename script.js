d8cd69b9080840b66f252351adc68ac9a4dcade8
Olha Sashchuk <olha.sashchuk@gmail.com>
2023-09-18 15:00:50 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,72 @@
+/*// 1 Тип даних: число
+let name = 'Olga';
+console.log(name);
+
+// 2 Тип даних: текст
+let age = 36;
+console.log(age);
+
+// 3 Тип даних: з великою кількістю цифр
+let bigNumber = 123456789n;
+console.log(bigNumber);
+console.log(typeof bigNumber);
+
+// 4 Тип даних: null
+let unValue = null;
+console.log(unValue);
+
+// 5 Тип даних: невизначене значення
+let country;
+console.log(country);
+
+// 6 Тип даних: об'єкт
+const student= {
+    name: 'Ivan',
+    age: 30,
+};
+console.log(student);
+console.log(typeof student);
+
+let myArray = ['Olga', 'Anna', 'Ivan'];
+console.log(myArray[0]);
+console.log (typeof myArray);
+
+// 7 Тип даних: boolean (логічний)
+const user1 = 'Olga';
+const user2 = 'Ivan';
+
+console.log(user1 == user2);
+console.log(typeof(user1 == user2));
+
+const number1 = 2023;
+const number2 = '2023';
+console.log(number1 == number2);
+console.log(typeof(number1 == number2));
+console.log(number1 === number2);
+console.log(typeof(number1 === number2));
+
+let areYouStudent = true;
+if (areYouStudent) {
+        console.log ('super');
+    } else {
+        console.log ('going to study');
+    };
+console.log (typeof areYouStudent);
+
+// 8 Тип даних: Symbol
+const postCode = Symbol('02192');
+console.log (postCode);
+console.log (typeof postCode);
+
+// 9 Тип даних: функція
+function test () {
+    console.log ('I am');
+    console.log ('student');
+}
+test ();
+console.log (typeof test);*/
+
+
+
+
+
